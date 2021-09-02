08f3506e29783f63fb2f70c2b01670a9b93c483a
wengison <venglar01@gmail.com>
2021-09-02 17:20:19 +0200

    problem se zobrazenim navbaru v mobil verzi - vyresen

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos > currentScrollPos || currentScrollPos == 0) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-80px";
